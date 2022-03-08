02fabd2348b987049ca8f8f894ca42265e97402e
Gethin Davies <grd.davies@gmail.com>
2022-03-08 02:07:12 +0000

    :sparkles: Constant time solution of problem01

--- problem01.hs
+++ problem01.hs
@@ -1,3 +1,13 @@
-main :: IO()
 -- Find the sum of all the multiples of 3 or 5 below 1000.
-main = putStr . show $ sum [x | x <- [1..999], (mod x 3 == 0) || (mod x 5 == 0)]
+main :: IO()
+main = putStr . show $ sumBelowLim 3 + sumBelowLim 5 - sumBelowLim 15
+-- 233168
+
+sumArith :: Integral a => a -> a -> a -> a
+sumArith a d n = n * (2*a + (n - 1)*d) `div` 2
+
+getMax :: Integer -> Integer
+getMax = div 999
+
+sumBelowLim :: Integer -> Integer
+sumBelowLim x = sumArith x x (getMax x)
