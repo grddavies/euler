cf973fe333e3c60a43a23b81da8944c870ece2b2
Gethin Davies <grd.davies@gmail.com>
2022-03-09 04:37:10 +0000

    :zap: Linear time+space solution to problem02

--- problem02.hs
+++ problem02.hs
@@ -1,12 +1,7 @@
 -- By considering the terms in the Fibonacci sequence whose values do not
 -- exceed four million, find the sum of the even-valued terms.
-fib :: Int -> Integer
-fib 1 = 1
-fib 2 = 2
-fib x = fib (x - 2) + fib (x - 1)
-
-fibSeq :: [Integer]
-fibSeq = map fib [1..]
+fibs :: [Integer]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 main :: IO()
-main = putStr . show $ sum $ filter even $ takeWhile ( < 4000000) fibSeq
+main = putStr . show $ sum $ filter even $ takeWhile ( < 4000000) fibs
