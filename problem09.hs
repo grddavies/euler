61f6b7c05eb82eb68bab4fcefde0ae9e48489e59
Gethin Davies <grd.davies@gmail.com>
2022-04-04 22:39:50 -0600

    :family_man_man_girl: Solve problem 09

---
+++ problem09.hs
@@ -0,0 +1,12 @@
+-- There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+-- Find the product abc.
+
+-- Euclid's formula for generating pythagorean triples:
+-- a = m^2 - n^2, b = 2mn, c = m^2 + n^2
+-- NB: Does not generate _all_ triples with only m, n
+
+sumVal :: Integer
+sumVal = 1000
+
+main :: IO ()
+main = print $ head [(x ^ 3 - y ^ 2)* (2 * x * y)*(x ^ 2 + y ^ 2) | x <- [2 ..], y <- [1 .. (x -1)], 2 * x ^ 2 + 2 * x * y == sumVal]
