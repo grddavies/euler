7bbf09335c304cbbab7a2a387c3e99493fcd86db
Gethin Davies <grd.davies@gmail.com>
2022-04-19 23:41:11 +0100

    :sparkles: Solve problem048

---
+++ problem048.hs
@@ -0,0 +1,12 @@
+-- The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+-- Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000
+import Control.Monad (join)
+
+powerSeries :: Integral b => b -> [b]
+powerSeries n = map (join (^)) [1..n]
+
+takeLastN :: Int -> [a] -> [a]
+takeLastN n = reverse . take n . reverse
+
+main :: IO ()
+main = putStrLn . takeLastN 10 $ show $ sum $ powerSeries 1000
