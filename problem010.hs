e59f81669387efe3701efd0a46552e4a964848b0
Gethin Davies <grd.davies@gmail.com>
2022-07-12 14:14:28 +0100

    :package: Create Primes module

--- problem010.hs
+++ problem010.hs
@@ -2,21 +2,7 @@
 -- Find the sum of all the primes below two million.
 
 import qualified Data.Map as M
-
-primes :: [Integer]
-primes = sieve [2 ..]
-
-
--- https://doi.org/10.1017/S0956796808007004
-sieve :: Integral a => [a] -> [a]
-sieve xs = sieve' xs M.empty
-  where
-    sieve' [] table = []
-    sieve' (x : xs) table = case M.lookup x table of
-      Nothing -> x : sieve' xs (M.insert (x * x) [x] table)
-      Just facts -> sieve' xs (foldl reinsert (M.delete x table) facts)
-      where
-        reinsert table prime = M.insertWith (++) (x + prime) [prime] table
+import Primes (primes)
 
 main :: IO ()
 main = print $ sum $ takeWhile (< 2000000) primes
--- problem046.hs
+++ problem046.hs
@@ -6,15 +6,13 @@ import Data.Foldable (find)
 import qualified Data.Map as M
 import qualified Control.Monad
 import Data.Maybe (fromJust)
+import Primes (primes)
 
 main :: IO ()
 main = putStrLn $ maybe "Goldbach was right!" show (find notGoldbachs odds)
 
 odds :: [Integer]
 odds = [3, 5 ..]
-
-primes :: [Integer]
-primes = sieve [2 ..]
 
 member :: Ord a => a -> [a] -> Bool
 member x xs = x `elem` takeWhile (<= x) xs
@@ -26,14 +24,3 @@ goldbachs :: Integer -> Bool
 goldbachs x = any (`member` primes) remainders where
   remainders = map (x - ) $ takeWhile (< x) doubledSquares
   doubledSquares = map ((2 *) . (^ 2)) [1..]
-
--- https://doi.org/10.1017/S0956796808007004
-sieve :: Integral a => [a] -> [a]
-sieve xs = sieve' xs M.empty
-  where
-    sieve' [] table = []
-    sieve' (x : xs) table = case M.lookup x table of
-      Nothing -> x : sieve' xs (M.insert (x * x) [x] table)
-      Just facts -> sieve' xs (foldl reinsert (M.delete x table) facts)
-      where
-        reinsert table prime = M.insertWith (++) (x + prime) [prime] table
