25b476b8cd5b966ad45b0b8ece35ffbc0b96175b
Gethin Davies <grd.davies@gmail.com>
2023-03-19 12:04:25 -0600

    Solve #070 - Euler Totient

--- Primes.hs
+++ Primes.hs
@@ -1,6 +1,7 @@
 module Primes where
 
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
+import qualified Data.List as L
 
 primes :: [Integer]
 primes = sieve [2 ..]
@@ -8,6 +9,25 @@ primes = sieve [2 ..]
 
 primesInt :: [Int]
 primesInt = sieve [2..]
+
+
+-- Prime factors by trial division of primes
+primeFactors n = factors n primesInt
+ where
+  factors 1 _                  = []
+  factors m (p:ps) | m < p*p   = [m]
+                   | r == 0    = p : factors q (p:ps)
+                   | otherwise = factors m ps
+   where (q,r) = quotRem m p
+
+-- List of tuples of prime factors and their exponents
+primeFactorsGroup n = map (\xs -> (head xs, length xs)) $ L.group $ primeFactors n
+
+-- Euler totient function
+eulerTotient n = totient n
+  where
+    totient 1 = 1
+    totient n = L.foldl1' (*) [p^(k-1) * (p-1) | (p, k) <- primeFactorsGroup n]
 
 -- https://doi.org/10.1017/S0956796808007004
 sieve :: Integral a => [a] -> [a]
@@ -19,3 +39,4 @@ sieve xs = sieve' xs M.empty
       Just facts -> sieve' xs (foldl reinsert (M.delete x table) facts)
       where
         reinsert table prime = M.insertWith (++) (x + prime) [prime] table
+
