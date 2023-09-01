a2f611da8d63c9a41ad79f6bfce13c619d1703cf
Gethin Davies <grd.davies@gmail.com>
2023-09-01 11:10:09 +0100

    Generalise solution for any 2 numbers

--- problem001.hs
+++ problem001.hs
@@ -1,12 +1,14 @@
 -- Find the sum of all the multiples of 3 or 5 below 1000.
-main :: IO()
-main = putStr . show $ sumBelowLim 3 + sumBelowLim 5 - sumBelowLim 15
+main :: IO ()
+main = print $ sumMultiplesBelow 3 5 1000
 
-sumArith :: Integral a => a -> a -> a -> a
-sumArith a d n = n * (2*a + (n - 1)*d) `div` 2
+sumArith :: (Integral a) => a -> a -> a -> a
+sumArith a d n = n * (2 * a + (n - 1) * d) `div` 2
 
-getMax :: Integer -> Integer
-getMax = div 999
-
-sumBelowLim :: Integer -> Integer
-sumBelowLim x = sumArith x x (getMax x)
+sumMultiplesBelow :: Integer -> Integer -> Integer -> Integer
+sumMultiplesBelow x y lim = sumX + sumY - sumLcm
+ where
+  sumX = sumBelowLim x 1000
+  sumY = sumBelowLim y 1000
+  sumLcm = sumBelowLim (lcm x y) 1000
+  sumBelowLim n lim = sumArith n n ((lim - 1) `div` n)
